e617b4d0b376f228bf7c32376035ec29b1a559d8
kurenai3110 <keke0001@hotmail.co.jp>
2017-05-03 00:19:19 +0900

    Update plot.py

--- TCO2017MMR1/plot.py
+++ TCO2017MMR1/plot.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 V = []
 E = []
 Score = []
-
+AllScore=0
 for line in lines:
     words = line.split()
 
@@ -19,23 +19,47 @@ for line in lines:
             E.append(int(words[5]))
     elif words[0] == 'Score':
         Score.append(float(words[2]))
-
+        AllScore += float(words[2])
+AllScore = AllScore/len(Score)
 VE = [E/V for E,V in zip(E,V)]
+Ave = [[] for i in range(101)]
+for i in range(len(Score)):
+    Ave[int(round(VE[i]*10))].append(Score[i])
+AveScore = []
+start=0
+for i in range(101):
+    if len(Ave[i])==0:
+        start+=0.1
+        continue
+    score = 0
+    for ave in Ave[i]:
+        score+=ave
+    score /= len(Ave[i])
+    AveScore.append(score)
+plt.plot(np.arange(start, 10.1, 0.1), np.array(AveScore), label='Score')
+plt.plot(np.arange(0, 22, 11), np.array([AllScore,AllScore]),label='AverageScore=' + '{:.2f}'.format(AllScore))
+plt.xlim(0.5,10.5)
+plt.xticks(np.arange(1,11,1))
+plt.legend(loc='best')
+#plt.ylabel('Score')
+plt.xlabel('NE / NV')
+plt.savefig('plot1.png')
+plt.show()
 
-V = V[::10]
-E = E[::10]
-VE = VE[::10]
-Score = Score[::10]
+V = V[:len(Score):7]
+E = E[:len(Score):7]
+VE = VE[:len(Score):7]
+Score = Score[::7]
 
 x = np.array(VE)
 #plt.xlim(10, 1000)
 y = np.array(Score)
-#plt.ylim(-0.1,1.1)
+plt.ylim(0.5,1.02)
 
 plt.ylabel('Score')
 plt.xlabel('NE / NV')
 
-plt.plot(x,y, 'o')
+plt.plot(x,y, 'o', markeredgecolor='k')
 
 plt.savefig('plot.png')
 
